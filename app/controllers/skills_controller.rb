a76c48091b6e7e8f87eeecdafe002567dedf0ddd
Brian Clary <baclary17@gmail.com>
2017-11-07 13:59:09 -0500

    filled out various actions including create, destroy, and update; added private method skill_params to work with the create and update methods

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,5 +1,5 @@
 class SkillsController < ApplicationController
-  before_action :set_skill, only [:show, :edit]
+  before_action :set_skill, only [:show, :edit, :update, :destroy]
 
   def index
     @skills = Skill.all
@@ -12,21 +12,26 @@ class SkillsController < ApplicationController
     @skill = Skill.new
   end
 
+  def create
+    Skill.create(restaurant_params)
+    redirect_to skills_path
+  end
+
   def edit
   end
 
-  def destroy
-  end
-
   def update
-  end
+    @skill.update(skill_params)
+    redirect_to skill_path(@skill)
 
-  def create
+  def destroy
+    @skill.destroy
+    redirect_to skills_path
   end
 
   private
   def skill_params
-    params.require(:restaurant).permit(:name, )
+    params.require(:skill).permit(:name, :description)
   end
 
   def set_skill
