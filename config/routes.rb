ad5de614ae455afe93ddb73e5bbfe40a0faa14fb
Brian Clary <baclary17@gmail.com>
2017-11-07 14:06:47 -0500

    tweaked line 11 in the routes file b/c we're using all 7 CRUD actions, not just select actions as formerly listed. I put a note in the bottom of the routes.rb file with the original code just in case I'm mistaken

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :skills, only: [:index, :show, :new, :create, :edit] do
+  resources :skills do
     resources :ateliers, only: [:show, :create, :new]
   end
+
   resources :users, only: [:index, :show, :new, :create, :update, :edit] do
     resources :reviews, only: [:show, :create, :new]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+# ***** Brian's Note *****
+# edited line 11. The following is the original line:
+# resources :skills, only: [:index, :show, :new, :create, :edit] do
