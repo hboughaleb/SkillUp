47683387c66f90cd81349daf2e0fa899389c9f69
Hamza Boughaleb <hamza.boughaleb@gmail.com>
2017-11-07 10:45:26 -0500

    completed validations for all models

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,8 @@ class User < ApplicationRecord
   has_many :skills, through: :ateliers
   has_many :ateliers, dependent: :destroy
   # has_attachment :photo
-
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
 end
