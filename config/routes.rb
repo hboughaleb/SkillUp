07e6314807792d940abfb521d4b8bcb7ed8f59f7
Hamza Boughaleb <hamza.boughaleb@gmail.com>
2017-11-08 00:09:24 -0500

    unpdated css component for nanner and cards alors with their partials, changes routes to match the app model, user profile stucture set, need a clean skill controller and view to make it work

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,17 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-
-  resources :skills, only: [:index, :show, :new, :create] do
-    resources :ateliers, only: [:show, :create, :new]
+  resources :users, only: [ :index, :show ] do
+    resources :skills, only: [ :new, :create ]
   end
-  resources :user, only: [:index, :show, :new, :create, :update, :edit] do
-    resources :skills, only: [:new, :create]
+
+  resources :skills, only: [ :index ] do
     resources :reviews, only: [:show, :create, :new]
   end
 
-  mount Attachinary::Engine => "/attachinary"
+  resources :ateliers, only: [:show, :edit ]
 
+  mount Attachinary::Engine => "/attachinary"
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
